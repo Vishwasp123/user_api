607d13bf205ca2a6ec2b2dc30f695792aaff7a4e
Vishwasp123 <vishawasparashar111@gmail.com>
2024-05-05 23:15:15 +0530

    Add users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,5 @@
 class UsersController < ApplicationController
 
-  # def create 
-  #   @user = User.new(user_params)
-  #   if @user.save 
-  #     token = encode_token(user_id: @user.id)
-  #      UserMailer.welcome_email(@user, token).deliver_now
-  #      # SendWelcomeEmailJob.perform_in(10.seconds, [@user.id, token]) 
-  #     render json: { token: token, message: 'User created successfully' }, status: :created
-  #   else 
-  #     render json: { error: @user.errors.full_messages }, status: :unprocessable_entity
-  #   end
-  # end
-
-
   def create
     @user = User.new(user_params)
     if @user.save
